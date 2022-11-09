3b5c2ae7f4470f560da0b6ee147126d6e8a1dd20
Valentina Salazar <valentinasalazarhh@gmail.com>
2022-11-08 20:22:35 -0500

    se cambió info de tema principal

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ let total2= 0;
 let total3= 0;
 const valorTotal = (a,b,c) => a + b + c
 
-let bienvenida = prompt('Bienvenida a Polett - Nail Spa. \n\n¿Quieres conocer nuestros servicios? \nIngresa "Si" para continuar');
+let bienvenidos = prompt('Bienvenidos a Hoja Santa, aquí encontrarás las mejores especias para tu cocina. \n\n¿Quieres conocer nuestros productos? \nIngresa "Si" para continuar');
 
-if (bienvenida == "si" || bienvenida == "Si" || bienvenida == "SI" || bienvenida == "sI"); {    
+if (bienvenidos == "si" || bienvenidos == "Si" || bienvenidos == "SI" || bienvenidos == "sI"); {    
     let nombre = (prompt("Primero queremos conocerte. ¿Cómo es tu nombre?"));
-    let continuar = Number(prompt("¡Hola "+nombre+"! Ahora, digita '1' para conocer nuestros servicios o digita '2' para salir"));
+    let continuar = Number(prompt("¡Hola "+nombre+"! Ahora, digita '1' para conocer nuestros productos o digita '2' para salir"));
     while(continuar != 2) {
-        let n = Number(prompt(nombre+" nos encanta tenerte aquí. Elige el servicio que deseas adquirir: \n1. Esmaltado tradicional manos: $20000 COP \n2. Esmaltado tradicional pies: $15000 COP \n3. Esmaltado semipermanente manos: $60000 COP \n4. Total \n5. Salir" ));
+        let n = Number(prompt(nombre+" nos encanta tenerte aquí. Elige el productos que deseas comprar: \n1. Pimienta negra: $40000 COP \n2. Chile seco: $15000 COP \n3. Azafrán en polvo: $10000 COP \n4. Total \n5. Salir" ));
         if(n==1){
-            let cantidad1 = Number(prompt("¡Ven con tus amigas! ¿Cuántos esmaltados tradicionales de manos deseas adquirir?"));
-            total1 = 20000*cantidad1;
+            let cantidad1 = Number(prompt("¿Cuántos tarritos de pimienta deseas adquirir?"));
+            total1 = 40000*cantidad1;
         }
         else if(n==2){
-            let cantidad2 = Number(prompt("¡Ven con tus amigas! ¿Cuántos esmaltados tradicionales de pies deseas adquirir?"));
+            let cantidad2 = Number(prompt("¿Cuántos tarritos de Chile seco deseas adquirir?"));
             total2 = 15000*cantidad2;
         }
         else if(n==3){
-            let cantidad3 = Number(prompt("¡Ven con tus amigas! ¿Cuántos esmaltados semipermanentes de manos deseas adquirir?"));
-            total3 = 60000*cantidad3;
+            let cantidad3 = Number(prompt("¿Cuántos tarritos de Azafrán en polvo deseas adquirir?"));
+            total3 = 10000*cantidad3;
         }
         else if(n==4){
             let total=valorTotal(total1,total2,total3);
@@ -33,3 +33,12 @@ if (bienvenida == "si" || bienvenida == "Si" || bienvenida == "SI" || bienvenida
         }
     }
 }
+
+
+// let productos=["Chile", "Epazote", "Azafran", "Pimienta", "Romero"] // 5...0-4
+// for(let i=0;i<productos.length;i++){
+//     console.log(productos[i]);
+// }
+// let productoGrande=[];
+// productoGrande[0]="Tamaño grande";
+// console.log(productoGrande)
